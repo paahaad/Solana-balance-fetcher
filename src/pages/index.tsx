329e59c4311bb1b9742ce641c58a019f234ce39e
parvatraj2 <parvat.raj2@gmail.com>
2023-05-02 10:35:36 +0530

    ✨Added alert for wrong balance

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,27 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import {PublicKey} from '@solana/web3.js'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import getBalanceUsingJSONRPC from '../service'
+import {getBalanceUsingJSONRPC, getBalanceUsingWeb3} from '../service'
 import React, { useState } from 'react'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   const monekeyAdd: string = '0xA9CB737826b9e2D4d4F88841148DA8B43944A3E8'
   const parvayAdd: string = '8VTXby1inY76KvwGcUFWWysBcHVT9GxhdHa2fYTwbxKj'
-  const [address, setAdress] = useState('')
+  const [address, setAdress] = useState<string>('')
   const [balance, setBalance] = useState<string>('')
   function handleBalanceFetch(){
-    const result = getBalanceUsingJSONRPC(address)
+    setBalance('')
+    // Change to String to PublicKey
+    if(address.length<parvayAdd.length){ 
+      alert('Wrong Addresss')
+      return
+    }
+    const publicKey = new PublicKey(address)
+
+    const result = getBalanceUsingWeb3(publicKey)
     result.then((r)=>{
       setBalance(String(r))
     }).catch((err)=>{
@@ -31,7 +40,7 @@ export default function Home() {
       <button onClick={handleBalanceFetch}>Check SOL Balance</button>
       <br />
       
-      {balance && <h3>Balacne: {balance}</h3>}
+      {balance && <h3>Balacne: {Number(balance)/1000000000}</h3>}
     </div>
     </>
   )
